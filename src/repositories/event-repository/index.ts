7eba84ea0083b76774affaff97faa7d5f2cef449
Marcio-VOT <marciofsantos65@gmail.com>
2023-05-19 16:57:08 -0300

    feat: event data on cache, redis.

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,17 @@
-import { prisma } from '@/config';
+import { prisma, redis } from '@/config';
+import { Event } from '@prisma/client';
 
-async function findFirst() {
-  return prisma.event.findFirst();
+const eventKey = 'event';
+
+async function findFirst(): Promise<Event> {
+  const cachedEvent = await redis.get(eventKey);
+
+  if (cachedEvent) return JSON.parse(cachedEvent);
+
+  const event = await prisma.event.findFirst();
+
+  await redis.setEx(eventKey, 86400, JSON.stringify(event));
+  return event;
 }
 
 const eventRepository = {
